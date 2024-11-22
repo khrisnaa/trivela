b755739cd0b83d61b21f29d403b43f35c910544b
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-11-22 17:29:26 +0800

    feat: create role permission seeder

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run() : void
+    {
+        $permissions = [
+            'manage categories',
+            'manage packages',
+            'manage transactions',
+            'manage package banks',
+            'checkout package',
+            'view orders'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission
+            ]);
+        }
+
+        $customerRole = Role::firstOrCreate([
+            'name' => 'customer',
+
+        ]);
+
+        $customerPermissions = [
+            'checkout package',
+            'view orders'
+        ];
+
+        $customerRole->syncPermissions($customerPermissions);
+
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin',
+
+        ]);
+
+        $user = User::create([
+            'name' => 'Super Admin',
+            'email' => 'super@admin.com',
+            'phone_number' => '6282244667983',
+            'avatar' => 'images/default-avatar.png',
+            'password' => bcrypt('loremipsum')
+        ]);
+
+        $user->assignRole($superAdminRole);
+    }
+}
