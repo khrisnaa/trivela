ad588a21020198beb8dfd8a41894d0e6ac28f9ca
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-11-27 14:58:48 +0800

    feat: create read function for categories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,45 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <div class="flex flex-row items-center justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Manage Categories') }}
             </h2>
-            <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+            <a href="{{ route('admin.categories.create') }}"
+                class="rounded-full bg-indigo-700 px-6 py-4 font-bold text-white">
                 Add New
             </a>
         </div>
     </x-slot>
-    
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
 
-             
-                <div class="item-card flex flex-row justify-between items-center">
-                    <div class="flex flex-row items-center gap-x-3">
-                        <img src=" " alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
-                        <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">cate name</h3>
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex flex-col gap-y-5 overflow-hidden bg-white p-10 shadow-sm sm:rounded-lg">
+
+                @forelse ($categories as $category)
+                    <div class="item-card flex flex-row items-center justify-between">
+                        <div class="flex flex-row items-center gap-x-3">
+                            <img src=" " alt="" class="h-[90px] w-[90px] rounded-2xl object-cover">
+                            <div class="flex flex-col">
+                                <h3 class="text-xl font-bold text-indigo-950">cate name</h3>
+                            </div>
                         </div>
-                    </div> 
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">date</h3>
+                        <div class="hidden flex-col md:flex">
+                            <p class="text-sm text-slate-500">Date</p>
+                            <h3 class="text-xl font-bold text-indigo-950">date</h3>
+                        </div>
+                        <div class="hidden flex-row items-center gap-x-3 md:flex">
+                            <a href=" " class="rounded-full bg-indigo-700 px-6 py-4 font-bold text-white">
+                                Edit
+                            </a>
+                            <form action=" " method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="rounded-full bg-red-700 px-6 py-4 font-bold text-white">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Edit
-                        </a>
-                        <form action=" " method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
-                                Delete
-                            </button>
-                        </form>
-                    </div>
-                </div>
-                
+                @empty
+                    <p>Belum ada data category</p>
+                @endforelse
 
             </div>
         </div>
