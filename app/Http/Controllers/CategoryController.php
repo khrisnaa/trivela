71e01e4c84f9b049f0f1588ac49adf855780377d
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-11-28 11:42:20 +0800

    feat: create delete category function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -80,6 +80,10 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        DB::transaction(function () use ($category) {
+            $category->delete();
+        });
+
+        return redirect()->route('admin.categories.index');
     }
 }
