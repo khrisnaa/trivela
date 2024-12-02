a493694e3df24b86b9e4e72f7e2faddabdc4c4c9
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-12-02 11:34:00 +0800

    feat: create list and create function for tour

--- app/Http/Controllers/PackageTourController.php
+++ app/Http/Controllers/PackageTourController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\PackageTour;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\StorePackageTourRequest;
 
 class PackageTourController extends Controller
 {
@@ -12,7 +16,8 @@ class PackageTourController extends Controller
      */
     public function index()
     {
-        //
+        $package_tours = PackageTour::orderByDesc('id')->paginate(10);
+        return view('admin.package_tours.index', compact('package_tours'));
     }
 
     /**
@@ -20,15 +25,43 @@ class PackageTourController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::orderByDesc('id')->get();
+        return view('admin.package_tours.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePackageTourRequest $request)
     {
-        //
+        //closure base transcation
+        DB::transaction(function () use ($request) {
+            $validated = $request->validated();
+
+            if ($request->hasFile('thumbnail')) {
+                $thumbnailPath = $request->file('thumbnail')->store('thumbnails/' . date('Y/m/d'), 'public');
+                $validated['thumbnail'] = $thumbnailPath;
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            //eloquent create
+            $packageTour = PackageTour::create($validated);
+
+            if ($request->hasFile('photos')) {
+                foreach ($request->file('photos') as $photo) {
+                    $photoPath = $photo->store('package_photos/' . date('Y/m/d'), 'public');
+
+                    //eloquent relation
+                    $packageTour->photos()->create([
+                        'photo' => $photoPath
+                    ]);
+                }
+            }
+        });
+
+        return redirect()->route('admin.package_tours.index');
+
     }
 
     /**
--- resources/views/admin/package_tours/index.blade.php
+++ resources/views/admin/package_tours/index.blade.php
@@ -1,43 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <div class="flex flex-row items-center justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Manage Tours') }}
             </h2>
-            <a href="#" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+            <a href="{{ route('admin.package_tours.create') }}"
+                class="rounded-full bg-indigo-700 px-6 py-4 font-bold text-white">
                 Add New
             </a>
         </div>
     </x-slot>
-    
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex flex-col gap-y-5 overflow-hidden bg-white p-10 shadow-sm sm:rounded-lg">
 
-
-                <div class="item-card flex flex-row justify-between items-center">
-                    <div class="flex flex-row items-center gap-x-3">
-                        <img src="#" alt="" class="rounded-2xl object-cover w-[120px] h-[90px]">
-                        <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">tour name</h3>
-                        <p class="text-slate-500 text-sm">tour category</p>
+                @forelse ($package_tours as $tour)
+                    <div class="item-card flex flex-row items-center justify-between">
+                        <div class="flex flex-row items-center gap-x-3">
+                            <img src="{{ Storage::url($tour->thumbnail) }}" alt=""
+                                class="h-[90px] w-[120px] rounded-2xl object-cover">
+                            <div class="flex flex-col">
+                                <h3 class="text-xl font-bold text-indigo-950">{{ $tour->name }}</h3>
+                                <p class="text-sm text-slate-500">{{ $tour->category->name }}</p>
+                            </div>
                         </div>
-                    </div> 
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Price</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">Rp 0</h3>
+                        <div class="hidden flex-col md:flex">
+                            <p class="text-sm text-slate-500">Price</p>
+                            <h3 class="text-xl font-bold text-indigo-950">Rp
+                                {{ number_format($tour->price, 0, ',', '.') }}</h3>
+                        </div>
+                        <div class="hidden flex-col md:flex">
+                            <p class="text-sm text-slate-500">Total Days</p>
+                            <h3 class="text-xl font-bold text-indigo-950">{{ $tour->days }} Days</h3>
+                        </div>
+                        <div class="hidden flex-row items-center gap-x-3 md:flex">
+                            <a href="" class="rounded-full bg-indigo-700 px-6 py-4 font-bold text-white">
+                                Manage
+                            </a>
+                        </div>
                     </div>
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Total Days</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">11 Days</h3>
-                    </div>
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href="#" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Manage
-                        </a>
-                    </div>
-                </div>
-                
+                @empty
+                    <p class="text-gray-500">No tours available at the moment.</p>
+                @endforelse
 
             </div>
         </div>
