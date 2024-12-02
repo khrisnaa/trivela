e683ef00f692a454faf4d3b3b0c3cafbd3fa5f1e
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-12-02 11:47:25 +0800

    fix: soft deletes

--- app/Models/PackageTour.php
+++ app/Models/PackageTour.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PackageTour extends Model
 {
-    use HasUuids;
+    use HasUuids, SoftDeletes;
 
     protected $fillable = [
         'name',
